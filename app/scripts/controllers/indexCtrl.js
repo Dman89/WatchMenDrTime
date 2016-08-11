233dc279f08236dd9f89b334d970402337d98ec5
Daniel Cudney <upnvapesd@gmail.com>
2016-08-11 00:36:03 -0700

    Save Code
    Save Code

--- app/scripts/controllers/indexCtrl.js
+++ app/scripts/controllers/indexCtrl.js
@@ -1,7 +1,13 @@
 'use strict';
 angular.module("drTimeWatchmen")
-.controller("indexCtrl", function($scope, timerService) {
-  timerService.getTime(function(res) {
-    $scope.countDownTimerDisplayNumber = res;
+.controller("indexCtrl", function($scope, timerService, $interval) {
+  timerService.getTime(function(h, m, s) {
+    var timerTimeToDisplay = timerService.convertTime(h,m,s);
+    timerService.fifteenSprint(timerTimeToDisplay, function(m, s) {
+      timerService.playTimer(m,s, function(res) {
+        console.log(res);
+        $scope.countDownTimerDisplayNumber = res;
+      })
+    })
   });
 });
